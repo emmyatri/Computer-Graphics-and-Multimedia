27855743d2f96c30f3c2a1e6082b4cdfea4f0db4
emmyatri <aelling6@students.kennesaw.edu>
2026-01-15 23:09:01 -0500

    Add project files.

---
+++ 2DSierpinskiTriangle/2DSierpinskiTriangle.js
@@ -0,0 +1,113 @@
+﻿/*
+ * Course: CS 4722
+ * Section: 01
+ * Name: Amelia Ellingson
+ * Professor: Alan Shaw
+ * Assignment #: Mod_1, Assignment_1, Lab_2
+ */
+
+
+"use strict";
+
+var canvas;
+var gl;
+
+var points = [];
+
+var numTimesToSubdivide = 0;
+
+var bufferId;
+
+window.onload = function init() {
+    canvas = document.getElementById("gl-canvas");
+
+    gl = canvas.getContext('webgl2', {});
+    if (!gl) { alert("WEBGL2 not available"); }
+
+    //
+    //  Initialize our data for the Sierpinski Gasket
+    //    
+
+    //
+    //  Configure WebGL
+    //
+    gl.viewport(0, 0, canvas.width, canvas.height);
+    gl.clearColor(1.0, 1.0, 1.0, 1.0);
+
+    //  Load shaders and initialize attribute buffers
+
+    let program = initShaders(gl, "vertex-shader", "fragment-shader");
+    gl.useProgram(program);
+
+    // Load the data into the GPU
+
+    bufferId = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
+    gl.bufferData(gl.ARRAY_BUFFER, 8 * Math.pow(3, 6), gl.STATIC_DRAW);
+
+
+
+    // Associate out shader variables with our data buffer
+
+    let vPosition = gl.getAttribLocation(program, "vPosition");
+    gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vPosition);
+
+
+
+
+    document.getElementById("textval").onchange =
+        function (event) {
+            numTimesToSubdivide = Number(document.getElementById('textval').value);
+            render();
+        };
+
+
+    render();
+};
+
+
+function triangle(a, b, c) {
+    points.push(a, b, c);
+}
+
+function divideTriangle(a, b, c, count) {
+
+    // check for end of recursion
+
+    if (count == 0) {
+        triangle(a, b, c);
+    }
+    else {
+
+        //bisect the sides
+
+        let ab = mix(a, b, 0.5);
+        let ac = mix(a, c, 0.5);
+        let bc = mix(b, c, 0.5);
+
+        --count;
+
+        // three new triangles
+        divideTriangle(a, ab, ac, count);
+        divideTriangle(c, ac, bc, count);
+        divideTriangle(b, bc, ab, count);
+    }
+}
+
+
+function render() {
+    let vertices = [
+        vec2(-1, -1),
+        vec2(0, 1),
+        vec2(1, -1)
+    ];
+    points = [];
+    divideTriangle(vertices[0], vertices[1], vertices[2],
+        numTimesToSubdivide);
+
+    gl.bufferSubData(gl.ARRAY_BUFFER, 0, flatten(points));
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.drawArrays(gl.TRIANGLES, 0, points.length);
+    points = [];
+}
---
+++ InnerSquare/InnerSquare.js
@@ -0,0 +1,87 @@
+﻿/*
+ * Course: CS 4722
+ * Section: 01
+ * Name: Amelia Ellingson
+ * Professor: Alan Shaw
+ * Assignment #: Mod_1, Assignment_1, Lab_1
+ */
+
+"use strict";
+
+var gl;
+var points;
+var length = 0.5;
+var height = 0.5;
+
+// Four Vertices
+var vertices = [
+    vec2(-length, -height),
+    vec2(-length, height),
+    vec2(length, height),
+    vec2(length, -height)
+];
+
+window.onload = function init() {
+    let canvas = document.getElementById("gl-canvas");
+
+    gl = canvas.getContext('webgl2', {});
+    if (!gl) { alert("WEBGL2 not available"); }
+
+    //
+    //  Configure WebGL
+    //
+    gl.viewport(0, 0, canvas.width, canvas.height);
+    gl.clearColor(1.0, 0.0, 0.0, 1.0);
+
+    //  Load shaders and initialize attribute buffers
+
+    let program = initShaders(gl, "vertex-shader", "fragment-shader");
+    gl.useProgram(program);
+
+    // Load the data into the GPU
+
+    let bufferId = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW);
+
+    // Associate out shader variables with our data buffer
+
+    let vPosition = gl.getAttribLocation(program, "vPosition");
+    gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vPosition);
+
+    document.getElementById("lengthval").oninput =
+
+        function (event) {
+            length = event.target.value;
+            vertices = [
+                vec2(-length, -height),
+                vec2(-length, height),
+                vec2(length, height),
+                vec2(length, -height)
+            ];
+            render();
+        };
+
+    document.getElementById("heightval").oninput =
+
+        function (event) {
+            height = event.target.value;
+            vertices = [
+                vec2(-length, -height),
+                vec2(-length, height),
+                vec2(length, height),
+                vec2(length, -height)
+            ];
+            render();
+        };
+
+    render();
+};
+
+
+function render() {
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW);
+    gl.drawArrays(gl.TRIANGLE_FAN, 0, 4);
+}
