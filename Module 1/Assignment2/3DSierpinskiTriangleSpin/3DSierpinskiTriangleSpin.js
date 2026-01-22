a5fbf33f1eef0b47999fbecb60c0ab1390570c9d
emmyatri <aelling6@students.kennesaw.edu>
2026-01-22 16:47:39 -0500

    assignment 2-mod1

--- Module 1/Assignment2/3DSierpinskiTriangleSpin/3DSierpinskiTriangleSpin.js
+++ Module 1/Assignment2/3DSierpinskiTriangleSpin/3DSierpinskiTriangleSpin.js
@@ -1,1 +1,140 @@
-﻿
+﻿/*
+ * Course: CS 4722
+ * Section: 01
+ * Name: Amelia Ellingson
+ * Professor: Alan Shaw
+ * Assignment #: Mod 1, Assignment 2, Lab 4
+ */
+
+"use strict";
+
+var canvas;
+var gl;
+
+var points = [];
+var colors = [];
+
+var NumTimesToSubdivide = 3;
+
+window.onload = function init() {
+    canvas = document.getElementById("gl-canvas");
+
+    gl = canvas.getContext('webgl2', {});
+    if (!gl) { alert("WEBGL2 not available"); }
+
+    //
+    //  Initialize our data for the Sierpinski Gasket
+    //
+
+    // First, initialize the vertices of our 3D gasket
+    // Four vertices on unit circle
+    // Intial tetrahedron with equal length sides
+
+    let vertices = [
+        vec3(0.0000, 0.0000, -1.0000),
+        vec3(0.0000, 0.9428, 0.3333),
+        vec3(-0.8165, -0.4714, 0.3333),
+        vec3(0.8165, -0.4714, 0.3333)
+    ];
+
+    divideTetra(vertices[0], vertices[1], vertices[2], vertices[3],
+        NumTimesToSubdivide);
+
+    //
+    //  Configure WebGL
+    //
+    gl.viewport(0, 0, canvas.width, canvas.height);
+    gl.clearColor(1.0, 1.0, 1.0, 1.0);
+
+    // enable hidden-surface removal
+
+    gl.enable(gl.DEPTH_TEST);
+
+    //  Load shaders and initialize attribute buffers
+
+    let program = initShaders(gl, "vertex-shader", "fragment-shader");
+    gl.useProgram(program);
+
+    // Create a buffer object, initialize it, and associate it with the
+    //  associated attribute variable in our vertex shader
+
+    let cBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(colors), gl.STATIC_DRAW);
+
+    let vColor = gl.getAttribLocation(program, "vColor");
+    gl.vertexAttribPointer(vColor, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vColor);
+
+    let vBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW);
+
+    let vPosition = gl.getAttribLocation(program, "vPosition");
+    gl.vertexAttribPointer(vPosition, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vPosition);
+
+    render();
+};
+
+function triangle(a, b, c, color) {
+
+    // add colors and vertices for one triangle
+
+    let baseColors = [
+        vec3(1.0, 0.0, 0.0),
+        vec3(0.0, 1.0, 0.0),
+        vec3(0.0, 0.0, 1.0),
+        vec3(0.0, 0.0, 0.0)
+    ];
+
+    colors.push(baseColors[color]);
+    points.push(a);
+    colors.push(baseColors[color]);
+    points.push(b);
+    colors.push(baseColors[color]);
+    points.push(c);
+}
+
+function tetra(a, b, c, d) {
+    // tetrahedron with each side using
+    // a different color
+
+    triangle(a, c, b, 0);
+    triangle(a, c, d, 1);
+    triangle(a, b, d, 2);
+    triangle(b, c, d, 3);
+}
+
+function divideTetra(a, b, c, d, count) {
+    // check for end of recursion
+
+    if (count === 0) {
+        tetra(a, b, c, d);
+    }
+
+    // find midpoints of sides
+    // divide four smaller tetrahedra
+
+    else {
+        let ab = mix(a, b, 0.5);
+        let ac = mix(a, c, 0.5);
+        let ad = mix(a, d, 0.5);
+        let bc = mix(b, c, 0.5);
+        let bd = mix(b, d, 0.5);
+        let cd = mix(c, d, 0.5);
+
+        --count;
+
+        divideTetra(a, ab, ac, ad, count);
+        divideTetra(ab, b, bc, bd, count);
+        divideTetra(ac, bc, c, cd, count);
+        divideTetra(ad, bd, cd, d, count);
+    }
+}
+
+
+function render() {
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    gl.drawArrays(gl.TRIANGLES, 0, points.length);
+}
--- Module 1/Assignment2/RotatingSquare/RotatingSquare.js
+++ Module 1/Assignment2/RotatingSquare/RotatingSquare.js
@@ -1,1 +1,87 @@
-﻿
+﻿/*
+ * Course: CS 4722
+ * Section: 01
+ * Name: Amelia Ellingson
+ * Professor: Alan Shaw
+ * Assignment #: Mod 1, Assignment 2, Lab 3
+ */
+
+
+"use strict";
+
+var canvas;
+var gl;
+
+var theta = 0;
+var dtheta = 0.1;
+var thetaLoc;
+var speed = 50;
+var dir = -1;
+var bufferId;
+var vertices;
+
+window.onload = function init() {
+    canvas = document.getElementById("gl-canvas");
+
+    gl = canvas.getContext('webgl2', {});
+    if (!gl) { alert("WEBGL2 not available"); }
+
+    //
+    //  Configure WebGL
+    //
+    gl.viewport(0, 0, canvas.width, canvas.height);
+    gl.clearColor(1.0, 1.0, 1.0, 1.0);
+
+    //  Load shaders and initialize attribute buffers
+    let program = initShaders(gl, "vertex-shader", "fragment-shader");
+    gl.useProgram(program);
+
+
+    vertices = [
+        vec2(0, 1),
+        vec2(-1, 0),
+        vec2(1, 0),
+        vec2(0, -1)
+    ];
+
+    // Load the data into the GPU
+    bufferId = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW);
+
+    // Associate our shader variables with our data buffer
+    let vPosition = gl.getAttribLocation(program, "vPosition");
+    gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vPosition);
+
+    // uniform variable declaration in init()
+    thetaLoc = gl.getUniformLocation(program, "theta");
+    gl.uniform1f(thetaLoc, theta);
+
+
+    document.getElementById("sliderval").oninput =
+        function (event) {
+            speed = Number(event.target.input);
+        };
+
+
+    document.getElementById("buttonval").onclick =
+        function () {
+            dir = -dir;
+        }
+
+    
+
+    render();
+
+};
+
+
+function render() {
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    theta += dir * dtheta * speed / 100;
+    gl.uniform1f(thetaLoc, theta)
+
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+    requestAnimFrame(render);
+}
