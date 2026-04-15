6d2003728f5f3d97d4479954057e69ac2db68fc7
emmyatri <aelling6@students.kennesaw.edu>
2026-04-15 12:32:12 -0400

    mod6 assignment 2 prep

---
+++ Module 6/Assignment2/BatmanCube/BatmanCube.js
@@ -0,0 +1,197 @@
+﻿/*
+ * Course: CS 4722
+ * Section: 01
+ * Name: Amelia Ellingson
+ * Professor: Alan Shaw
+ * Assignment #: Mod6, Assignment2, Lab3
+ */
+
+"use strict";
+
+var canvas;
+var gl;
+var program;
+
+var numVertices = 36;
+
+var texSize = 64;
+var imgSize = 64;
+var numChecks = 4;
+
+var checkerImage = new Uint8Array(4 * imgSize * imgSize);
+
+var pointsArray = [];
+var colorsArray = [];
+var texCoordsArray = [];
+
+var texCoord = [
+    vec2(0, 0),
+    vec2(0, 1),
+    vec2(1, 1),
+    vec2(1, 0)
+];
+
+var vertices = [
+    vec4(-0.5, -0.5, 0.5, 1.0),
+    vec4(-0.5, 0.5, 0.5, 1.0),
+    vec4(0.5, 0.5, 0.5, 1.0),
+    vec4(0.5, -0.5, 0.5, 1.0),
+    vec4(-0.5, -0.5, -0.5, 1.0),
+    vec4(-0.5, 0.5, -0.5, 1.0),
+    vec4(0.5, 0.5, -0.5, 1.0),
+    vec4(0.5, -0.5, -0.5, 1.0)
+];
+
+var vertexColors = [
+    vec4(1.0, 0.0, 1.0, 1.0),  // magenta
+    vec4(0.0, 0.0, 0.0, 1.0),  // black
+    vec4(0.0, 0.0, 1.0, 1.0),  // blue
+    vec4(0.0, 1.0, 1.0, 1.0),  // cyan
+    vec4(1.0, 1.0, 0.0, 1.0),  // yellow
+    vec4(1.0, 1.0, 1.0, 1.0),  // white
+    vec4(0.0, 1.0, 0.0, 1.0),  // green
+    vec4(1.0, 0.0, 0.0, 1.0)   // red
+];
+
+var xAxis = 0;
+var yAxis = 1;
+var zAxis = 2;
+var axis = xAxis;
+
+var theta = [45.0, 45.0, 45.0];
+var thetaLoc;
+
+
+window.onload = function init() {
+    canvas = document.getElementById("gl-canvas");
+
+    gl = canvas.getContext('webgl2', {});
+    if (!gl) { alert("WEBGL2 not available"); }
+
+    gl.viewport(0, 0, canvas.width, canvas.height);
+    gl.clearColor(1.0, 1.0, 1.0, 1.0);
+
+    gl.enable(gl.DEPTH_TEST);
+
+    //
+    //  Load shaders and initialize attribute buffers
+    //
+    program = initShaders(gl, "vertex-shader", "fragment-shader");
+    gl.useProgram(program);
+
+    for (let i = 0; i < imgSize; i++) {
+        for (let j = 0; j < imgSize; j++) {
+            let patchx = Math.floor(i / (imgSize / numChecks));
+            let patchy = Math.floor(j / (imgSize / numChecks));
+            let c;
+            c = (patchx % 2 ^ patchy % 2) ? 255 : 0;
+            checkerImage[4 * i * imgSize + 4 * j] = c;
+            checkerImage[4 * i * imgSize + 4 * j + 1] = c;
+            checkerImage[4 * i * imgSize + 4 * j + 2] = c;
+            checkerImage[4 * i * imgSize + 4 * j + 3] = 255;
+        }
+    }
+
+    colorCube();
+
+    let cBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(colorsArray), gl.STATIC_DRAW);
+    let vColor = gl.getAttribLocation(program, "vColor");
+    gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vColor);
+
+    let vBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(pointsArray), gl.STATIC_DRAW);
+    let vPosition = gl.getAttribLocation(program, "vPosition");
+    gl.vertexAttribPointer(vPosition, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vPosition);
+
+    let tBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, tBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(texCoordsArray), gl.STATIC_DRAW);
+    let vTexCoord = gl.getAttribLocation(program, "vTexCoord");
+    gl.vertexAttribPointer(vTexCoord, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vTexCoord);
+
+    configureTexture(checkerImage);
+
+    thetaLoc = gl.getUniformLocation(program, "theta");
+
+    document.getElementById("ButtonX").onclick =
+        function () {
+            axis = xAxis;
+        };
+
+    document.getElementById("ButtonY").onclick =
+        function () {
+            axis = yAxis;
+        };
+
+    document.getElementById("ButtonZ").onclick =
+        function () {
+            axis = zAxis;
+        };
+
+    render();
+}
+
+function configureTexture(image) {
+    let texture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, texSize, texSize, 0,
+        gl.RGBA, gl.UNSIGNED_BYTE, image);
+    gl.generateMipmap(gl.TEXTURE_2D);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER,
+        gl.NEAREST_MIPMAP_LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+
+    let textureLoc = gl.getUniformLocation(program, "sampTexture");
+    gl.activeTexture(gl.TEXTURE0);
+    gl.uniform1i(textureLoc, 0);
+}
+
+function colorCube() {
+    quad(1, 0, 3, 2);
+    quad(2, 3, 7, 6);
+    quad(3, 0, 4, 7);
+    quad(5, 1, 2, 6);
+    quad(4, 5, 6, 7);
+    quad(5, 4, 0, 1);
+}
+
+function quad(a, b, c, d) {
+    pointsArray.push(vertices[a]);
+    colorsArray.push(vertexColors[c]);
+    texCoordsArray.push(texCoord[0]);
+
+    pointsArray.push(vertices[b]);
+    colorsArray.push(vertexColors[c]);
+    texCoordsArray.push(texCoord[1]);
+
+    pointsArray.push(vertices[c]);
+    colorsArray.push(vertexColors[c]);
+    texCoordsArray.push(texCoord[2]);
+
+    pointsArray.push(vertices[a]);
+    colorsArray.push(vertexColors[c]);
+    texCoordsArray.push(texCoord[0]);
+
+    pointsArray.push(vertices[c]);
+    colorsArray.push(vertexColors[c]);
+    texCoordsArray.push(texCoord[2]);
+
+    pointsArray.push(vertices[d]);
+    colorsArray.push(vertexColors[c]);
+    texCoordsArray.push(texCoord[3]);
+}
+
+function render() {
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    theta[axis] += 2.0;
+    gl.uniform3fv(thetaLoc, theta);
+    gl.drawArrays(gl.TRIANGLES, 0, numVertices);
+    requestAnimFrame(render);
+}
---
+++ Module 6/Assignment2/MoonOrbit/MoonOrbit.js
@@ -0,0 +1,182 @@
+﻿/*
+ * Course: CS 4722
+ * Section: 01
+ * Name: Amelia Ellingson
+ * Professor: Alan Shaw
+ * Assignment #: Mod6, Assignment2, Lab4
+ */
+
+"use strict";
+
+var canvas;
+var gl;
+var program;
+
+var latitudeBands = 30;
+var longitudeBands = 30;
+var radius = 2;
+
+var pointsArray = [];
+var texCoordsArray = [];
+
+var vBuffer;
+var tBuffer;
+
+var vPosition;
+var vTexCoord;
+
+var modelViewMatrix, projectionMatrix;
+var modelViewMatrixLoc, projectionMatrixLoc;
+var texture, textureLoc;
+
+var up = vec3(0.0, 1.0, 0.0);
+var at = vec3(0.0, 0.0, 0.0);
+var eye = vec3(0.0, 0.0, 1.0);
+
+var near = -10;
+var far = 10;
+var left = -3.0;
+var right = 3.0;
+var ytop = 3.0;
+var bottom = -3.0;
+
+window.onload = function init() {
+    canvas = document.getElementById("gl-canvas");
+
+    gl = canvas.getContext('webgl2', {});
+    if (!gl) { alert("WEBGL2 not available"); }
+
+    gl.viewport(0, 0, canvas.width, canvas.height);
+    gl.clearColor(1.0, 1.0, 1.0, 1.0);
+
+    gl.enable(gl.DEPTH_TEST);
+
+    //
+    //  Load shaders and initialize attribute buffers
+    //
+    program = initShaders(gl, "vertex-shader", "fragment-shader");
+    gl.useProgram(program);
+
+    createSphereMap();
+
+    // Create vertex buffer and vPosition attribute
+    vBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(pointsArray), gl.STATIC_DRAW);
+    vPosition = gl.getAttribLocation(program, "vPosition");
+    gl.vertexAttribPointer(vPosition, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vPosition);
+
+    // Create texture buffer and vTexCoord attribute
+    tBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, tBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(texCoordsArray), gl.STATIC_DRAW);
+    vTexCoord = gl.getAttribLocation(program, "vTexCoord");
+    gl.vertexAttribPointer(vTexCoord, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vTexCoord);
+
+    // Get buffer locations for the following shader variables
+    projectionMatrixLoc = gl.getUniformLocation(program, "projectionMatrix");
+    modelViewMatrixLoc = gl.getUniformLocation(program, "modelViewMatrix");
+    textureLoc = gl.getUniformLocation(program, "sampTexture");
+
+    projectionMatrix = ortho(left, right, bottom, ytop, near, far);
+    gl.uniformMatrix4fv(projectionMatrixLoc, false, flatten(projectionMatrix));
+
+    loadImage(document.getElementById(document.getElementById('imageVal').value));
+
+    document.getElementById("imageVal").onchange =
+        function (event) {
+            loadImage(document.getElementById(event.target.value));
+            createSphereMap();
+        };
+
+    render();
+}
+
+// Create SphereMap by filling pointsArray, normalsArray and texCoordsArray
+function createSphereMap() {
+    pointsArray = [];
+    texCoordsArray = [];
+
+    let phi1, phi2, sinPhi1, sinPhi2, cosPhi1, cosPhi2;
+    let theta1, theta2, sinTheta1, sinTheta2, cosTheta1, cosTheta2;
+    let p1, p2, p3, p4, u1, u2, v1, v2, uv1, uv2, uv3, uv4;
+    let r = radius;
+
+    // For each latitudinal band determine phi's value
+    for (let latNumber = 1; latNumber <= latitudeBands; latNumber++) {
+        phi1 = Math.PI * (latNumber - 1) / latitudeBands;
+        sinPhi1 = Math.sin(phi1);
+        cosPhi1 = Math.cos(phi1);
+
+        phi2 = Math.PI * latNumber / latitudeBands;
+        sinPhi2 = Math.sin(phi2);
+        cosPhi2 = Math.cos(phi2);
+
+        // For each longitudinal band determine theta's value and other calculations
+        for (let longNumber = 1; longNumber <= longitudeBands; longNumber++) {
+            theta1 = 2 * Math.PI * (longNumber - 1) / longitudeBands;
+            sinTheta1 = Math.sin(theta1);
+            cosTheta1 = Math.cos(theta1);
+
+            theta2 = 2 * Math.PI * longNumber / longitudeBands;
+            sinTheta2 = Math.sin(theta2);
+            cosTheta2 = Math.cos(theta2);
+
+            p1 = vec4(cosTheta1 * sinPhi1 * r, cosPhi1 * r, sinTheta1 * sinPhi1 * r, 1.0);
+            p2 = vec4(cosTheta2 * sinPhi1 * r, cosPhi1 * r, sinTheta2 * sinPhi1 * r, 1.0);
+            p3 = vec4(cosTheta1 * sinPhi2 * r, cosPhi2 * r, sinTheta1 * sinPhi2 * r, 1.0);
+            p4 = vec4(cosTheta2 * sinPhi2 * r, cosPhi2 * r, sinTheta2 * sinPhi2 * r, 1.0);
+
+            pointsArray.push(p1);
+            pointsArray.push(p2);
+            pointsArray.push(p3);
+            pointsArray.push(p2);
+            pointsArray.push(p4);
+            pointsArray.push(p3);
+
+            u1 = 1 - ((longNumber - 1) / longitudeBands);
+            u2 = 1 - (longNumber / longitudeBands);
+            v1 = 1 - ((latNumber - 1) / latitudeBands);
+            v2 = 1 - (latNumber / latitudeBands);
+
+            uv1 = vec2(u1, v1);
+            uv2 = vec2(u2, v1);
+            uv3 = vec2(u1, v2);
+            uv4 = vec2(u2, v2);
+
+            texCoordsArray.push(uv1);
+            texCoordsArray.push(uv2);
+            texCoordsArray.push(uv3);
+            texCoordsArray.push(uv2);
+            texCoordsArray.push(uv4);
+            texCoordsArray.push(uv3);
+        }
+    }
+}
+
+function loadImage(image) {
+    texture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_NEAREST);
+    gl.generateMipmap(gl.TEXTURE_2D);
+
+    gl.activeTexture(gl.TEXTURE0);
+    gl.uniform1i(textureLoc, 0);
+}
+
+function render() {
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    modelViewMatrix = lookAt(eye, at, up);
+
+    gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix));
+
+    gl.drawArrays(gl.TRIANGLES, 0, pointsArray.length);
+
+    requestAnimFrame(render);
+}
